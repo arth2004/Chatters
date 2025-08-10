2abfff176381d17382a702e428fe6a150f1fc54c
Arth <arthagrawal72@gmail.com>
2025-08-10 23:11:37 +0530

    added render base url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
+const BASE_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api"
+    : "https://chatters-rv90.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
